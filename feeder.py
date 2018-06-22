4d6665cf661fb3365aa5d0beffc745668254c0f8
Sadiq <shuiab.sadiq@intel.com>
2018-06-22 09:11:47 +0530

    Table creation code added

--- feeder.py
+++ feeder.py
@@ -1,21 +1,32 @@
-import sql
+import sql as s
 import common as c
 
 #Load all the scrips first
 def load_scrips():
-	query = "SELECT * FROM scrips"
-	c.pr("I","Loading All Scrips",1)
-	
+	global scrips
+	c.pr("I","Loading All Scrips",0)
+	scrips = s.sql_hash("scrips","scrip","sector:status:is_fetch")
 	return
-
-
 
 def init():
 	load_scrips()
+	check_tables()
+	load_scrips()
 	return	
 
+def check_tables():
+	c.pr("I","Checking If Destination Table's Exists",0)
+	for scrip in scrips:
+		c.pr("I","Checking For Table "+scrip,1)
+		qry = "SELECT * FROM information_schema.tables WHERE table_schema = 'stocki'  AND table_name = '"+scrip+"' LIMIT 1;"
+		if s.rcnt(qry):
+			c.pr("I",scrip+" Table Exists",1)
+		else:
+			c.pr("I",scrip+" Table Needs To Be Created",1)
+			s.create_table(scrip,"time:DT,timestamp:TS,open:FL,low:FL,high:FL,close:FL,volume:IN")
+	return
 #Script Flow Starts Here
 
-#scrips 		= {}
+scrips 		= {}
 init()
 #Flow End Here.
--- sql.py
+++ sql.py
@@ -5,6 +5,32 @@ def sql_conn():
 	db = sql.connect("localhost","root","","stocki")
 	return db
 
+def sql_hash(table,key,cols):
+    query = ""
+    ret   = {}
+    vals  = cols.split(":")
+    col   = key+","+",".join(vals)
+    query = "SELECT "+col+" FROM "+table
+    c.pr("S",query,1)
+
+    db_obj  = sql_conn()
+    cursor  = db_obj.cursor()
+    try:
+        cursor.execute(query)
+        results = cursor.fetchall()
+        for row in results:
+            k       = row[0]
+            ct      = 1
+            ret[k]  = {}  
+            for cl in vals:
+                ret[k][cl] = row[ct]
+                ct = ct + 1 
+    except (sql.Error, sql.Warning) as e:
+        print("-E- Query Failed")   
+        print(e)
+        db_obj.rollback()
+    return ret
+
 def rcnt(qry):
     db_obj  = sql_conn()
     cursor  = db_obj.cursor()
@@ -29,4 +55,29 @@ def execQuery(qry):
         print("-E- Query Failed")   
         print(e)
         db_obj.rollback() 
+    return
+
+def create_table(name,schema):
+    c.pr("I","Creating Table "+name,1)
+    var_map = {"FL" : "FLOAT" , "IN" : "INT(11)" , "DT": "DATETIME" ,"TS" : "TIMESTAMP" ,"VC" : "VARCHAR","TX":"TEXT"}
+    query   = "CREATE TABLE `"+name+"` ("
+    scharr  = schema.split(",")
+    for sch in scharr:
+        tmp = sch.split(":")
+        col = tmp[0]
+        dt  = tmp[1]
+        if dt == "VC":
+            size  = tmp[3]
+            query = query+"`"+col+"` "+var_map[dt] + "("+size+") NOT NULL,"
+        else:
+            query = query+"`"+col+"` "+var_map[dt] + " NOT NULL,\n"
+    query = query[0:-2]+")"
+    
+    qry = "SELECT * FROM information_schema.tables WHERE table_schema = 'stocki'  AND table_name = '"+name+"' LIMIT 1;"
+    execQuery(query)
+    if rcnt(qry):
+        c.pr("I",str(name+" Table Created"),1)
+        execQuery("UPDATE scrips SET status='YES' WHERE scrip='"+name+"'")
+    else:
+        c.pr("W",str(name+" Table Not Created"),1)
     return
